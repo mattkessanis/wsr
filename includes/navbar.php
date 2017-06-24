e580f1e9d41814fd761fa73c7bdd61829d4c42cc
Kewal <kewal@Kewals-MacBook-Pro.local>
2017-06-24 21:07:19 +1000

    homepage almost ready

---
+++ includes/navbar.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">
+        <img class="logo" alt="Western Sydney Racing" src="assets/images/logo.png" />
+      </a>
+      <p class="navbar-text">WESTERN SYDNEY RACING</p>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right ">
+        <li class="active"><a href="#">HOME</a></li>
+        <li><a href="#">TEAM</a></li>
+        <li><a href="#">CAR</a></li>
+        <li><a href="#">SPONSORS</a></li>
+        <li><a href="#">NEWS</a></li>
+        <li><a href="#">JOIN</a></li>
+        <li><a href="#">CONTACT</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'includes/header.php' ?>
+<title>Western Sydney Racing</title>
+<?php include 'includes/styles_scripts.php' ?>
+</head>
+<body>
+  <?php include 'includes/navbar.php' ?>
+  <div class="hero-image">
+
+  </div>
+
+
+
+<?php include 'includes/footer.php' ?>
